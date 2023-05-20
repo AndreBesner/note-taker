884edfbae2236e36c901e7fac90df1f03a2a3a02
Andre Besner <besner.andre@gmail.com>
2023-05-19 21:10:16 -0400

    get data saving to a json file, to be fixed and added to db file

--- server.js
+++ server.js
@@ -9,36 +9,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
-// you need to fill all api routes
-
 // HTML routes
 //this is what loads when user goes to main page of site
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'assets', 'index.html'));
-  });
+});
 
   //this loads when user clicks "get started"
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'assets', 'notes.html'));
 });
 
+//get notes API
 app.get('/api/notes', (req, res)=> {
   res.sendFile(path.join(__dirname, './db/db.json'));
 });
-
-
-
-//get notes API
 // tbd 
 
 
-//post
-//post note api
-// tbd
+// //post
+app.post('/api/notes', (req, res)=> {
+  const { title, text } = req.body
+
+  const newNote = {
+    title,
+    text
+  }
+
+  const noteString = JSON.stringify(newNote);
+
+  // Write the string to a file
+  fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+  err
+    ? console.error(err)
+    : console.log(
+        `Review forhas been written to JSON file`
+      )
+);
+})
 
 //delete (extra cred)
+// app.delete('/api/notes', (req, res)=> {
+//   req.sendFile(path.join(__dirname, './db/db.json'));
+// });
 
 //listen this one easy
 app.listen(PORT, () => {
